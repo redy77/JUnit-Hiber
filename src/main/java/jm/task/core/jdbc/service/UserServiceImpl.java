edcba0d7aafe8de1e7d0a5c05a76042a4b634426
nikitanesterenkos@gmail.com <piramida741147>
2021-08-10 11:14:39 +0300

    created a connection

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,30 +1,41 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl extends Util implements UserService {
+
+    Connection connection = getConnection();
+
+    @Override
     public void createUsersTable() {
 
     }
 
+    @Override
     public void dropUsersTable() {
 
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
 
     }
 
+    @Override
     public void removeUserById(long id) {
 
     }
 
+    @Override
     public List<User> getAllUsers() {
         return null;
     }
 
+    @Override
     public void cleanUsersTable() {
 
     }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final String URL = "jdbc:mysql://localhost:3306/taskjdbc?";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root-123";
+
+    public Connection getConnection(){
+        Connection connection = null;
+            try {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException throwable) {
+                throwable.printStackTrace();
+            }
+            return connection;
+    }
+
 }
