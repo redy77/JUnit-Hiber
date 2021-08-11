ab561ad208f0e0ecd6522f87dc2e59cca61cab9a
Viktor Bakakin <redy@yandex.ru>
2021-08-11 14:55:55 +0300

    check_2

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,13 +1,13 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Objects;
-
-@Table
+@Entity
+@Table (name = "newUser")
 public class User {
     @Id
+    @GeneratedValue
+    @Column
     private Long id;
 
    @Column
